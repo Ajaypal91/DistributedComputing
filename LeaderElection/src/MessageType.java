37bd23a1a4e113fbf67c67de7de1ac3b561fd86a
Ajaypal91 <erajaypal91@gmail.com>
2018-01-02 22:36:55 -0800

    adding new files

---
+++ LeaderElection/src/MessageType.java
@@ -0,0 +1,12 @@
+/**
+ * Messages that can be passed
+ * @author Sample
+ */
+public enum  MessageType {
+	/**
+	 * Indicates that a computation event is being performed
+	 */
+    IDENTIFIER,
+    TERMINATION
+    
+}
